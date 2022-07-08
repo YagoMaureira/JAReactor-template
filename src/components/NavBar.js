42d22b3531ae006e87e6f024476443085fbf7450
nico-olivares22 <nicolaso2604@gmail.com>
2022-07-08 18:01:12 -0300

    adding icon image in header and replace original favicon.ico

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import JAReactor from "../assets/JAReactor.png";
 
 export const NavBar = () => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">JAReactor</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-      </div>
+    <nav className="navbar bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="">
+                <img src={JAReactor} alt="" width={30} height={24} className="d-inline-block align-text-top" />
+                JAReactor
+            </a>
+        </div>
     </nav>
     )
