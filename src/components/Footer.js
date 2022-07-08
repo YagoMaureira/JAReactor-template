dc65681e8d0cf6618ac91997b50459e80ca78eaa
yago <yago98maureira@gmail.com>
2022-07-08 12:19:38 -0300

    React front template

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+
+export const Footer = () =>{
+    return (
+        <footer className="text-center text-white fixed-bottom" style={{backgroundColor: '#21081a'}}>
+          <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
+            © 2022 JAReactor
+          </div>
+        </footer>
+      );
+}
---
+++ src/components/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const MainPage = () => (
+  <div className="container">
+    <h1>Home Page JAReactor</h1>
+  </div>
+    
+    )
