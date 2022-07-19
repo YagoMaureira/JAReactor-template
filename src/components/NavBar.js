033403c4ad8cbc21b7d68d8344fc83956b6ba33a
yago <yago98maureira@gmail.com>
2022-07-19 09:43:21 -0300

    Fix NavBar button to homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import JAReactor from "../assets/JAReactor.png";
 export const NavBar = () => (
     <nav className="navbar bg-dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="">
+            <a className="navbar-brand" href="/">
                 <img src={JAReactor} alt="" width={30} height={24} className="d-inline-block align-text-top" />
                 JAReactor
             </a>
