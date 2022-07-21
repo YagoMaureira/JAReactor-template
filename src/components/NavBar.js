477a88a30d101363ad1bb21ad6ea61673157c233
Santiago Riera <rierasanti@hotmail.com>
2022-07-21 20:36:35 -0300

    Adding new logo and favicon ico

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,7 @@ export const NavBar = () => (
     <nav className="navbar bg-dark">
         <div className="container-fluid">
             <a className="navbar-brand" href="/">
-                <img src={JAReactor} alt="" width={30} height={24} className="d-inline-block align-text-top" />
-                JAReactor
+                <img src={JAReactor} alt="" width={225} height={60} className="d-inline-block align-text-top" />
             </a>
         </div>
     </nav>
