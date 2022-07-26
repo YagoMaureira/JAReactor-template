e1d76ccefbeb8e0ca60341f458a7428d27f17f9c
yago <yago98maureira@gmail.com>
2022-07-26 20:16:28 -0300

    Add Link in NavBar button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,11 @@
 import React from "react";
 import JAReactor from "../assets/JAReactor.png";
+import {Link} from "react-router-dom";
 
 export const NavBar = () => (
     <nav className="navbar bg-dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-                <img src={JAReactor} alt="" width={225} height={60} className="d-inline-block align-text-top" />
-            </a>
+        <Link className="navbar-brand" to={"/"}><img src={JAReactor} alt="" width={225} height={60} className="d-inline-block align-text-top" /></Link>
         </div>
     </nav>
     )
