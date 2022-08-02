624cb5a661946600557d9ce5ebd42ea54a094ea3
yago <yago98maureira@gmail.com>
2022-08-02 20:14:08 -0300

    Add margin top in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 
 export const Footer = () =>{
     return (
+      <div className="container-fluid footer-margin">
         <footer className="text-center text-white fixed-bottom" style={{backgroundColor: '#21081a'}}>
           <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
             © 2022 JAReactor
           </div>
         </footer>
+      </div>
       );
 }
